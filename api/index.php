55a0f03a819bef3c115d7872e6afe94d93bb47dc
Innocent <dusinnocent57@gmail.com>
2025-04-21 16:51:57 +0200

    Simplify Vercel configuration and update PHP entry point

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Forward Vercel requests to normal index.php
-require __DIR__ . '/../public/index.php';
+// Load composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Load Laravel
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
